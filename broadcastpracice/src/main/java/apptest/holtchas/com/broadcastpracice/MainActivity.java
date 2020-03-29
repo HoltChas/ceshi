7948959b1725c792602cee0513769e04bf5852f0
HoltChas <haocong_gentleman@163.com>
2020-03-29 09:49:16 +0800

    测试

---
+++ broadcastpracice/src/main/java/apptest/holtchas/com/broadcastpracice/MainActivity.java
@@ -0,0 +1,24 @@
+package apptest.holtchas.com.broadcastpracice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.force_offline);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.broadcastbestpractice.FORCE_OFFLINE");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
